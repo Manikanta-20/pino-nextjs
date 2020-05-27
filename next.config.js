4a6e51eb339f4768828d35ad6224496c245a70ca
Manikanta <khambhampatin@gmail.com>
2020-05-27 15:56:39 +0530

    inital commit

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 
 const webpack = require('webpack');
 const{join}=require('path');
+require('dotenv').config();
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
   });
-  module.exports = {
+  module.exports = withBundleAnalyzer({
+   
     webpack(config, options) {
       return config;
     }
-  };
+  });
